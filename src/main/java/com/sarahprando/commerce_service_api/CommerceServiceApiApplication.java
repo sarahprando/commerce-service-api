ef0c958a34f2fd3aa5190a4462cc3e8c97154cde
Sarah Prando <sarahprando40@gmail.com>
2026-07-08 19:42:49 -0300

    chore: initial project setup

---
+++ src/main/java/com/sarahprando/commerce_service_api/CommerceServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.sarahprando.commerce_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceServiceApiApplication.class, args);
+	}
+
+}
