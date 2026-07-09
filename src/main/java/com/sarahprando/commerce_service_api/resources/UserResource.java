29b2e18bfab50ca28dea5845bf6fb50d94aa52d5
Sarah Prando <sarahprando40@gmail.com>
2026-07-09 08:54:19 -0300

    feat: service layer, component registration

--- src/main/java/com/sarahprando/commerce_service_api/resources/UserResource.java
+++ src/main/java/com/sarahprando/commerce_service_api/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.sarahprando.commerce_service_api.resources;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sarahprando.commerce_service_api.entities.User;
+import com.sarahprando.commerce_service_api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+
+	private final UserService service;
+
+	UserResource(UserService service) {
+		this.service = service;
 	}
-	
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
- 
---
+++ src/main/java/com/sarahprando/commerce_service_api/services/UserService.java
@@ -0,0 +1,28 @@
+package com.sarahprando.commerce_service_api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.sarahprando.commerce_service_api.entities.User;
+import com.sarahprando.commerce_service_api.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	private final UserRepository repository;
+
+	UserService(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
