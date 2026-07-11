e3e8c14fbdc11ffe6c1ecf99367fe60f81f36ea8
Sarah Prando <sarahprando40@gmail.com>
2026-07-11 09:51:14 -0300

    feat: implement global exception handling with custom exceptions

--- src/main/java/com/sarahprando/commerce_service_api/services/UserService.java
+++ src/main/java/com/sarahprando/commerce_service_api/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sarahprando.commerce_service_api.entities.User;
